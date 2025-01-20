284b49fbd8200afa4b10a282df1b7233f46fbe98
João Vitor <joaovitorso071@gmail.com>
2025-01-20 20:11:52 -0300

    feat: Adição da model para formulario

---
+++ API-ConectaCiencia/API-ConectaCiencia/models/FormularioModel.cs
@@ -0,0 +1,17 @@
+﻿using API_ConectaCiencia.Model;
+
+namespace API_ConectaCiencia.models
+{
+    public class FormularioModel
+    {
+        public class FormularioTema
+        {
+            public int IdSugestaoTema { get; set; }
+            public string Nome { get; set; }
+            public string Email { get; set; }
+            public string Tema { get; set; }
+            public int Id_Categoria { get; set; }
+            public CategoriaModel Categoria { get; set; }
+        }
+    }
+}
