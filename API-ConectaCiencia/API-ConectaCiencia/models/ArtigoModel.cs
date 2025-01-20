4e79f83586b843a9be120767bf0b6eee6e818f2e
João Vitor <joaovitorso071@gmail.com>
2025-01-20 19:56:15 -0300

    feat: Adição a model para artigo

---
+++ API-ConectaCiencia/API-ConectaCiencia/models/ArtigoModel.cs
@@ -0,0 +1,13 @@
+﻿using API_ConectaCiencia.Model;
+
+namespace API_ConectaCiencia.models
+{
+    public class ArtigoModel
+    {
+        public int Id_Artigo { get; set; }
+        public DateTime Data { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public CategoriaModel Categoria { get; set; }
+    }
+}
