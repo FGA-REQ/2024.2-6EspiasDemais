a88e98637981c36faf134cbca589e37c7785c0c0
xzxjesse <jessicaeveline121@gmail.com>
2025-01-22 07:47:12 -0300

    feat: adiciona Repository para Formulario de Artigo

--- API-ConectaCiencia/API-ConectaCiencia/repositories/FormularioRepository.cs
+++ API-ConectaCiencia/API-ConectaCiencia/repositories/FormularioRepository.cs
@@ -35,5 +35,26 @@ namespace API_ConectaCiencia.repositories
                 }
             }
         }
+
+        public async Task AdicionarArtigo(FormularioModel.FormularioArtigo formularioArtigo)
+        {
+            var sql = @"INSERT INTO FormularioArtigo (nome, email, titulo, conteudo, id_categoria)
+                        VALUES (@nome, @email, @titulo, @conteudo, @id_categoria)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@nome", formularioArtigo.Nome);
+                    command.Parameters.AddWithValue("@email", formularioArtigo.Email);
+                    command.Parameters.AddWithValue("@titulo", formularioArtigo.Titulo);
+                    command.Parameters.AddWithValue("@conteudo", formularioArtigo.Conteudo);
+                    command.Parameters.AddWithValue("@id_categoria", formularioArtigo.Id_Categoria);
+
+                    connection.Open();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
     }
 }
