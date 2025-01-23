4bf9029d086b2238c80879f48893b9a27f95c233
Ruan-Carvalho <ruansobreira11@gmail.com>
2025-01-23 19:46:12 -0300

    dev: corrige o namespace

--- WebForms-ConectaCiencia/WebForms-ConectaCiencia/Model/Formularios.cs
+++ WebForms-ConectaCiencia/WebForms-ConectaCiencia/Model/Formularios.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProjetoFinal_DotNET.Model
+namespace WebForms_ConectaCiencia.Model
 {
     public class Formularios
     {
