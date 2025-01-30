dc281242576a9e7a29c7886a80b7887e5bda3f75
João Vitor <joaovitorso071@gmail.com>
2025-01-30 16:29:29 -0300

    dev: Adição da logica para adicionar usuarios

--- API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
+++ API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
@@ -55,6 +55,35 @@ namespace API_ConectaCiencia.repositories
         }
         return usuario;
     }
+    public int AdicionarUsuario(UsuarioModel usuario)
+    {
+        var sql = @"INSERT INTO Usuarios (Nome, Email, Senha) 
+                    OUTPUT INSERTED.Id_Usuario VALUES (@Nome, @Email, @Senha)";
+        var parameters = new List<SqlParameter>
+        {
+            new SqlParameter("@Nome", usuario.Nome),
+            new SqlParameter("@Email", usuario.Email),
+            new SqlParameter("@Senha", usuario.Senha)
+        };
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddRange(parameters.ToArray());
+                    var idUsuario = (int)command.ExecuteScalar();
+                    return idUsuario;
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception("Erro ao acessar o banco de dados.", ex);
+        }
+    }
 }
 
 
