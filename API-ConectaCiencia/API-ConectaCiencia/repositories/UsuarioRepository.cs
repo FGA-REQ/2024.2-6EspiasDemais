3dea1783b71540e9c92de0229c5a07ace0bdfa62
João Vitor <joaovitorso071@gmail.com>
2025-01-30 15:40:32 -0300

    dev: Adição da conexão na repositorio do usuario

---
+++ API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace API_ConectaCiencia.repositories 
+{
+    private readonly string _connectionString;
+    public UsuarioRepository(string connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("ConnectionString sem valor válido");
+        }
+        _connectionString = connectionString;
+    }
+}
+
+
