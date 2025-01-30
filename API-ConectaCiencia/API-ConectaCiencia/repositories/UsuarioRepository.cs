391e306baee8cd87acfcc328a8cace577dd61e4c
João Vitor <joaovitorso071@gmail.com>
2025-01-30 17:07:09 -0300

    dev: Correção da UsuarioRepository

--- API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
+++ API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
@@ -4,84 +4,87 @@ using Microsoft.Data.SqlClient;
 
 namespace API_ConectaCiencia.repositories 
 {
-    private readonly string _connectionString;
-    public UsuarioRepository(string connectionString)
+    public class UsuarioRepository
     {
-        if (string.IsNullOrEmpty(connectionString))
+        private readonly string _connectionString;
+        public UsuarioRepository(string connectionString)
         {
-            throw new Exception("ConnectionString sem valor válido");
-        }
-        _connectionString = connectionString;
-    }
-    public UsuarioModel ObterUsuarioPorEmailESenha(string email, string senha)
-    {
-        UsuarioModel usuario = null;
-        var sql = @"SELECT * FROM Usuarios 
-                    WHERE Email = @Email AND Senha = @Senha";
-        var parameters = new List<SqlParameter>
-        {
-            new SqlParameter("@Email", email),
-            new SqlParameter("@Senha", senha)
-        };
-
-        try
-        {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            if (string.IsNullOrEmpty(connectionString))
             {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddRange(parameters.ToArray());
+                throw new Exception("ConnectionString sem valor válido");
+            }
+            _connectionString = connectionString;
+        }
+        public UsuarioModel ObterUsuarioPorEmailESenha(string email, string senha)
+        {
+            UsuarioModel usuario = null;
+            var sql = @"SELECT * FROM Usuarios 
+                        WHERE Email = @Email AND Senha = @Senha";
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@Email", email),
+                new SqlParameter("@Senha", senha)
+            };
 
-                    using (SqlDataReader reader = command.ExecuteReader())
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        if (reader.Read())
+                        command.Parameters.AddRange(parameters.ToArray());
+
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            usuario = new UsuarioModel
+                            if (reader.Read())
                             {
-                                Id_Usuario = reader.GetInt32(reader.GetOrdinal("Id_Usuario")),
-                                Nome = reader.GetString(reader.GetOrdinal("Nome")),
-                                Email = reader.GetString(reader.GetOrdinal("Email")),
-                                Senha = reader.GetString(reader.GetOrdinal("Senha"))
-                            };
+                                usuario = new UsuarioModel
+                                {
+                                    Id_Usuario = reader.GetInt32(reader.GetOrdinal("Id_Usuario")),
+                                    Nome = reader.GetString(reader.GetOrdinal("Nome")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    Senha = reader.GetString(reader.GetOrdinal("Senha"))
+                                };
+                            }
                         }
                     }
                 }
             }
-        }
-        catch (SqlException ex)
-        {
-            throw new Exception("Erro ao acessar o banco de dados.", ex);
-        }
-        return usuario;
-    }
-    public int AdicionarUsuario(UsuarioModel usuario)
-    {
-        var sql = @"INSERT INTO Usuarios (Nome, Email, Senha) 
-                    OUTPUT INSERTED.Id_Usuario VALUES (@Nome, @Email, @Senha)";
-        var parameters = new List<SqlParameter>
-        {
-            new SqlParameter("@Nome", usuario.Nome),
-            new SqlParameter("@Email", usuario.Email),
-            new SqlParameter("@Senha", usuario.Senha)
-        };
-
-        try
-        {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            catch (SqlException ex)
             {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                throw new Exception("Erro ao acessar o banco de dados.", ex);
+            }
+            return usuario;
+        }
+        public int AdicionarUsuario(UsuarioModel usuario)
+        {
+            var sql = @"INSERT INTO Usuarios (Nome, Email, Senha) 
+                        OUTPUT INSERTED.Id_Usuario VALUES (@Nome, @Email, @Senha)";
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@Nome", usuario.Nome),
+                new SqlParameter("@Email", usuario.Email),
+                new SqlParameter("@Senha", usuario.Senha)
+            };
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    command.Parameters.AddRange(parameters.ToArray());
-                    var idUsuario = (int)command.ExecuteScalar();
-                    return idUsuario;
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddRange(parameters.ToArray());
+                        var idUsuario = (int)command.ExecuteScalar();
+                        return idUsuario;
+                    }
                 }
             }
-        }
-        catch (SqlException ex)
-        {
-            throw new Exception("Erro ao acessar o banco de dados.", ex);
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro ao acessar o banco de dados.", ex);
+            }
         }
     }
 }
