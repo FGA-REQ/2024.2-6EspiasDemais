a50d93f169a1b0870d168d9c666f22bef95e4fc4
João Vitor <joaovitorso071@gmail.com>
2025-01-30 16:58:12 -0300

    dev: Criação da conexão com a repositorio de usuario

---
+++ API-ConectaCiencia/API-ConectaCiencia/controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_ConectaCiencia.controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsuarioController
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("conexao");
+            _usuarioRepository = new UsuarioRepository(connectionString);
+        }
+    }
+}
