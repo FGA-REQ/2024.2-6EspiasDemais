3150571055f0502f57fa4f78a018824153e36b7a
João Vitor <joaovitorso071@gmail.com>
2025-01-30 16:05:07 -0300

    dev: Adição da logica para obter usuario por email e senha

--- API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
+++ API-ConectaCiencia/API-ConectaCiencia/repositories/UsuarioRepository.cs
@@ -1,4 +1,8 @@
-﻿namespace API_ConectaCiencia.repositories 
+﻿using API_ConectaCiencia.models;
+using API_ConectaCiencia.Model;
+using Microsoft.Data.SqlClient;
+
+namespace API_ConectaCiencia.repositories 
 {
     private readonly string _connectionString;
     public UsuarioRepository(string connectionString)
@@ -8,6 +12,48 @@
             throw new Exception("ConnectionString sem valor válido");
         }
         _connectionString = connectionString;
+    }
+    public UsuarioModel ObterUsuarioPorEmailESenha(string email, string senha)
+    {
+        UsuarioModel usuario = null;
+        var sql = @"SELECT * FROM Usuarios 
+                    WHERE Email = @Email AND Senha = @Senha";
+        var parameters = new List<SqlParameter>
+        {
+            new SqlParameter("@Email", email),
+            new SqlParameter("@Senha", senha)
+        };
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddRange(parameters.ToArray());
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            usuario = new UsuarioModel
+                            {
+                                Id_Usuario = reader.GetInt32(reader.GetOrdinal("Id_Usuario")),
+                                Nome = reader.GetString(reader.GetOrdinal("Nome")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                Senha = reader.GetString(reader.GetOrdinal("Senha"))
+                            };
+                        }
+                    }
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw new Exception("Erro ao acessar o banco de dados.", ex);
+        }
+        return usuario;
     }
 }
 
